cff89e8c7a98346a568c22e5aabe01fee709fa58
Cezar Milani <cezarmilani@gmail.com>
2023-11-07 23:33:00 -0300

    funções sql e chamadas do servidor terminadas

--- server.js
+++ server.js
@@ -8,26 +8,131 @@ app.use(express.static('public'));
 
 app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 var func = require('./sqlfuncoes');
 
-app.get('/aluno', function (req, res) {
-    func.getAlunos(function (err, rows) {
-       if (err) {
-          res.json(err);
-       } else {
-          console.log('Enviando resposta');
-          res.json(rows);
-       }
-    });
- });
+app.get('/professores', function (req, res) {
+   func.getProfessor(function (err, rows) {
+      if (err) {
+         res.json(err);
+      } else {
+         res.json(rows);
+      }
+   });
+});
 
- var server = app.listen(3000, function () {
+app.get('/disciplinas', function (req, res) {
+   func.getDisciplina(function (err, rows) {
+      if (err) {
+         res.json(err);
+      } else {
+         res.json(rows);
+      }
+   });
+});
 
-    var host = server.address().address;
-    var port = server.address().port;
- 
-    console.log(`Example app listening at http://%s:%s`, host, port);
- 
- });
+app.get('/turmas', function (req, res) {
+   func.getTurmas(function (err, rows) {
+      if (err) {
+         res.json(err);
+      } else {
+         res.json(rows);
+      }
+   });
+});
+
+app.get('/turmascomvaga', function (req, res) {
+   func.getTurmasComVaga(function (err, rows) {
+      if (err) {
+         res.json(err);
+      } else {
+         res.json(rows);
+      }
+   });
+});
+
+app.get('/alunosnaturma', function (req, res) {
+   func.getAlunosDaTurmas(function (err, rows) {
+      if (err) {
+         res.json(err);
+      } else {
+         res.json(rows);
+      }
+   });
+});
+
+app.get('/alunosforaturma', function (req, res) {
+   func.getAlunosForaTurma(function (err, rows) {
+      if (err) {
+         res.json(err);
+      } else {
+         res.json(rows);
+      }
+   });
+});
+
+app.post('/inseriralunoturma', function (req, res) {
+   func.insertAlunoTurma(req.body, function (err, rs) {
+      if (err) {
+         res.json(err);
+      } else {
+         func.decrementaLimiteVagas(req.body.id_turma, function (err, rs) {
+            if (err) {
+               res.json(err);
+            } else {
+               res.writeHead(200, { 'Content-Type': 'application/json' });
+               res.end('{ "msg": "Inserido com sucesso" }');
+            }
+         });
+      }
+   });
+});
+
+app.post('/criarturma', function (req, res) {
+   func.insertTurma(req.body, function (err, rs) {
+      if (err) {
+         res.json(err);
+      } else {
+         res.writeHead(200, { 'Content-Type': 'application/json' });
+         res.end('{ "msg": "Turma criada" }');
+      }
+   });
+});
+
+app.delete('/deletarturma', function (req, res) {
+   func.deleteTurma(req.body, function (err, rs) {
+      if (err) {
+         res.json(err);
+      } else {
+         res.writeHead(200, { 'Content-Type': 'application/json' });
+         res.end('{ "msg": "Turma deletada" }');
+      }
+   });
+});
+
+app.delete('/deletaralunodaturma', function (req, res) {
+   func.deleteAlunoTurma(req.body, function (err, rs) {
+      if (err) {
+         res.json(err);
+      } else {
+         func.incrementaLimite(req.body.id_turma, function (err, rs) {
+            if (err) {
+               res.json(err);
+            } else {
+               res.writeHead(200, { 'Content-Type': 'application/json' });
+               res.end('{ "msg": "Aluno deletado da turma" }');
+            }
+         });
+      }
+   });
+});
+
+var server = app.listen(3000, function () {
+
+   var host = server.address().address;
+   var port = server.address().port;
+
+   console.log(`Example app listening at http://%s:%s`, host, port);
+
+});
--- sqlfuncoes.js
+++ sqlfuncoes.js
@@ -1,22 +1,52 @@
 var db = require('./sqlconexao');
 
 var BancoDados = {
-    getProfessor: function(callback) {
-        return db.query("SELECT * FROM professores", null, callback);
+    getProfessor: function (callback) {
+        return db.query("SELECT professores.id_prof, professores.nome_prof, professores.sobrenome_prof FROM professores LEFT JOIN turmas ON professores.id_prof = turmas.id_prof WHERE turmas.id_prof IS NULL", null, callback);
     },
 
-    getDisciplina: function(callback) {
-        return db.query("SELECT * FROM disciplina", null, callback);
+    getDisciplina: function (callback) {
+        return db.query("SELECT disciplinas.id_disciplina, disciplinas.nome_disciplina FROM disciplinas LEFT JOIN turmas ON disciplinas.id_disciplina = turmas.id_disciplina WHERE turmas.id_disciplina IS NULL", null, callback);
     },
 
-    getAlunos: function (callback) {
-        return db.query("SELECT * FROM alunos",
-            null, callback);
+    getTurmas: function (callback) {
+        return db.query("SELECT * FROM turmas", null, callback);
     },
 
-    addAluno: function (aluno, callback) {
-        return db.query("INSERT INTO alunos (id_aluno, nome_aluno, sobrenome_aluno) VALUES (0,?,?)",
-            [aluno.id, aluno.nome, aluno.sobrenome], callback);
+    getTurmasComVaga: function (callback) {
+        return db.query("SELECT * FROM turmas WHERE limite_vagas > 0", null, callback);
+    },
+
+    getAlunosDaTurmas: function (idTurma, callback) {
+        return db.query("SELECT alunos.id_aluno, alunos.nome_aluno, alunos.sobrenome_aluno FROM alunos INNER JOIN turma_alunos ON alunos.id_aluno = turma_alunos.id_aluno WHERE turma_alunos.id_turma = ?", [idTurma], callback);
+    },
+
+    getAlunosForaTurma: function (idTurma, callback) {
+        return db.query("SELECT alunos.id_aluno, alunos.nome_aluno, alunos.sobrenome_aluno FROM alunos LEFT JOIN turma_alunos ON alunos.id_aluno = turma_alunos.id_aluno WHERE turma_alunos.id_turma != ? OR turma_alunos.id_turma IS NULL", [idTurma], callback);
+    },
+
+    insertAlunoTurma: function (alunoTurma, callback) {
+        return db.query("INSERT INTO turma_alunos (id_turma, id_aluno) VALUES (0,0)", [alunoTurma.id_turma, alunoTurma.id_aluno], callback);
+    },
+
+    insertTurma: function (turma, callback) {
+        return db.query("INSERT INTO turmas (id_turma, id_disciplina, id_prof, turno, dia, limite_vagas) VALUES (0,0,0,?,?,0)", [turma.id_turma, turma.id_disciplina, turma.id_prof, turma.turno, turma.dia, turma.limite_vagas], callback);
+    },
+
+    deleteAlunoTurma: function (alunoTurma, callback) {
+        return db.query("DELETE FROM turma_alunos WHERE id_turma = ? AND id_aluno = ?", [alunoTurma.id_turma, alunoTurma.id_aluno], callback);
+    },
+
+    deleteTurma: function (idTurma, callback) {
+        return db.query("DELETE FROM turmas WHERE id_turma = ?", [idTurma], callback);
+    },
+
+    decrementaLimite: function (idTurma, callback) {
+        return db.query("UPDATE turmas SET limite_vagas = limite_vagas - 1 WHERE id_turma = ?", [idTurma], callback);
+    },
+
+    incrementaLimite: function (idTurma, callback) {
+        return db.query("UPDATE turmas SET limite_vagas = limite_vagas + 1 WHERE id_turma = ?", [idTurma], callback);
     }
 };
 
